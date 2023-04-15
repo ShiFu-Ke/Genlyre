03f59fbbd6aa0cc4b1fb3dd69cd0e17566ca1036
zhangjie <zhangjie1493@163.com>
2023-04-15 17:02:19 +0800

    实现自动演奏

---
+++ Lib/log.py
@@ -0,0 +1,18 @@
+# @ Created with PyCharm Community Edition
+# @ Author KeShiFu
+# @ Date 2023/04/13
+# @ Time 18:06
+import time
+import logging  # logging默认值打印warning以上级别的信息
+import traceback  # 将原始报错的信息返回出来
+
+
+class Log:
+    def __init__(self, LogName):
+        logging.basicConfig(level='DEBUG', filename=LogName, filemode='a+')
+
+    @staticmethod
+    def addLog():
+        error = traceback.format_exc()
+        print(error)
+        logging.error(time.strftime('%y-%m-%d %H:%M:%S') + "\n" + error + '-------------- \n')
--- keyMap/test.ahk
+++
@@ -1,4 +0,0 @@
-Numpad1::A
-Numpad2::S
-Numpad3::Dl
-Numpad4::F
